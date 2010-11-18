268e48d8957beb0da6d9ca2184359c43bc8a708b
Steve Clement <steve@localhost.lu>
2010-11-18 21:59:44 +0100

    Added the main code for the ticketing project All .asc files are encrypted
    Former-commit-id: cae09d9bf50e7db89f59bb7b9d268462c7f6d33c

---
+++ ticket/ticket.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>64222</title>
+</head>
+<body>
+<strong>Me:</strong> A<br/>
+<small>Sent:
+<?php print(date("M d")); ?>
+</small><br/>
+<span style="color:blue;text-decoration:underline;font-weight:bold;">64222</span>:
+<?php
+print("non-random-hash");
+?><br/>
+Expire<br/>
+<?php
+print(date("d-m H:i",time()+7200));
+?><br/>
+1.50 euro (A)<br/>
+Billet courte dur&eacute;e<br/>
+<br/>
+Emis:
+<?php
+print(date("H:i"));
+?><br/>
+<small>Sent:
+<?php print(date("M d")); ?>
+</small>
+</body>
+<html>
