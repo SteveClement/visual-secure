7ec14b15002501923846fabe0d288cc75d36c6b2
Steve Clement <steve@Steve-Clements-MacBook-Air.local>
2011-05-31 18:10:07 +0200

    Added more SMS samples test tweaks on 3letter.py
    Former-commit-id: 2e252ea622f1ceb90b2593e4cbe54cd4b2ddce86

--- py-twats/3letter.py
+++ py-twats/3letter.py
@@ -2,37 +2,34 @@
 
 import itertools
 from string import ascii_lowercase
-import twitter
+#import twitter
 import datetime, time
-from pysqlite2 import dbapi2 as sqlite
-
+#from pysqlite2 import dbapi2 as sqlite
+import urllib2
 
 # Database connection handling
-connection = sqlite.connect('3letter.db')
-connection.isolation_level = None
-cursor = connection.cursor()
+#connection = sqlite.connect('3letter.db')
+#connection.isolation_level = None
+#cursor = connection.cursor()
 
-query = u'INSERT INTO data VALUES ("%s", "%s")' % (user, now)
+#query = u'INSERT INTO data VALUES ("%s", "%s")' % (user, now)
 
 count = 0
 
-api = twitter.Api()
-
 # We now have a string of the alphabet in ascii_lowercase
 complete_charset = ascii_lowercase + '_0123456789'
-
 product=itertools.product(list(complete_charset), repeat=3)
 
 for s in product:
   # Current time stamp
-  now = time.strftime("%Y-%m-%d %H:%M")
+  #now = time.strftime("%Y-%m-%d %H:%M")
   user = ''.join(s)
   try:
-    print "Firing request for" + user
-    api.GetUser(user)
+    url = "http://www.twitter.com/" + user
+    result = urllib2.urlopen(url)
   except:
-    print user + "is still available"
-    cursor.execute(query)
+    print user + " is still available"
+    #cursor.execute(query)
   count += 1
   print count + "of 50653"
-  time.sleep(30)
+  #time.sleep(30)
